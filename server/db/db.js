46e484acf26a59a1508cfbf79ed62043c02d0538
Johnrambo <barrars@gmail.com>
2020-03-11 22:37:37 -0700

    switch to local mongo to test ingredients index

--- server/db/db.js
+++ server/db/db.js
@@ -1,20 +1,20 @@
 const mongoose = require('mongoose')
 var logger = require('../helpers/mylogger')
 
-// mongoose.connect('mongodb://localhost/helloFresh', {
-//   useNewUrlParser: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false,
-//   useUnifiedTopology: true
-// })
+mongoose.connect('mongodb://localhost/helloFresh', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+})
 
-mongoose
-  .connect('mongodb://blurr123:blurr123@ds125073.mlab.com:25073/jumpstart', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  })
+// mongoose
+//   .connect('mongodb://blurr123:blurr123@ds125073.mlab.com:25073/jumpstart', {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true
+//   })
 
   .then(() => {
     logger.log('Connected to MongoDB')
