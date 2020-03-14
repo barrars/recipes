a032a16009c0b1a789ad15a5f614156af5a9833d
Johnrambo <barrars@gmail.com>
2020-03-13 19:54:39 -0700

    testing pi mongo

--- server/db/db.js
+++ server/db/db.js
@@ -7,14 +7,21 @@ var logger = require('../helpers/mylogger')
 //   useFindAndModify: false,
 //   useUnifiedTopology: true
 // })
+// for the pi
+mongoose.connect('mongodb://localhost/fresh', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+})
 
-mongoose
-  .connect('mongodb://blurr123:blurr123@ds125073.mlab.com:25073/jumpstart', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  })
+// mongoose
+//   .connect('mongodb://blurr123:blurr123@ds125073.mlab.com:25073/jumpstart', {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true
+//   })
 
   .then(() => {
     logger.log('Connected to MongoDB')
