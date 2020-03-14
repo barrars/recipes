7127c1740a339b82015d48d92dfe78b711b72ee0
Johnrambo <barrars@gmail.com>
2020-03-13 20:01:28 -0700

    testing pi mongo

--- server/routes/recipeRouter.js
+++ server/routes/recipeRouter.js
@@ -6,7 +6,6 @@ const Recipe = require('../models/recipeModel')
 // const RecipeController = require('../controllers/recipeController.js')
 // '/api/recipe'
 router.get('/:ingredient', async (req, res, next) => {
-  console.time('search2')
   const str = req.params.ingredient
   logger.log(':ingredient route hit ' + str)
   const makeArr = (str) => {
@@ -20,15 +19,12 @@ router.get('/:ingredient', async (req, res, next) => {
   // const result = await RecipeController.findByIngredients(ingredient)
   const result = {}
   console.log(makeArr(str))
-  console.time('search')
   result.results = await Recipe.find({ 'ingredients.name': { $all: makeArr(str) } }).select('cardLink headline category.slug ingredients.name name imagePath prepTime')
   // result.results = await Recipe.find({ $text: { $search: str } })
-  console.timeEnd('search')
   // result.count = await Recipe.countDocuments({ 'ingredients.name': { $all: makeArr(str) } })
   // logger.log(JSON.stringify(result, null, 2))
   // res.json(result)
   res.send(result)
-  console.timeEnd('search2')
 })
 
 module.exports = router
