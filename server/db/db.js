1a9af1f6a2e66cd1875156353609979bcf138e78
Johnrambo <barrars@gmail.com>
2020-03-21 02:52:45 -0700

    add recipe routes and

--- server/db/db.js
+++ server/db/db.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose')
 var logger = require('../helpers/mylogger')
 
 // local development
-mongoose.connect('mongodb://localhost/helloFresh', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-})
+// mongoose.connect('mongodb://localhost/helloFresh', {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+//   useUnifiedTopology: true
+// })
 // for the pi
 // mongoose.connect('mongodb://localhost/fresh', {
 //   useNewUrlParser: true,
@@ -16,13 +16,13 @@ mongoose.connect('mongodb://localhost/helloFresh', {
 //   useUnifiedTopology: true
 // })
 // live
-// mongoose
-//   .connect('mongodb://blurr123:blurr123@ds125073.mlab.com:25073/jumpstart', {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false,
-//     useUnifiedTopology: true
-//   })
+mongoose
+  .connect('mongodb://blurr123:blurr123@ds125073.mlab.com:25073/jumpstart', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
 
   .then(() => {
     logger.log('Connected to MongoDB')
